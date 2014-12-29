180f07fa50c42faae130c6724347efdb764ee157
jesse keane <me@jessekeane.me>
2014-12-29 12:02:18 -0800

    Emit on change, deep copy objects
    * Only emit on change
    * Deep copy all objects on input and output
    * Add keypath access
    * Fix nested `.wait()`

--- README.md
+++ README.md
@@ -3,84 +3,121 @@
 [![Build Status](http://img.shields.io/travis/urbanairship/objectstate/master.svg?style=flat)](https://travis-ci.org/urbanairship/objectstate)
 [![npm install](http://img.shields.io/npm/dm/objectstate.svg?style=flat)](https://www.npmjs.org/package/objectstate)
 
+## Overview
+
 `objectstate` exports a function that constructs a stream. The stream is
 designed to be the source of truth for the state of a system. It represents
-state as a JavaScript object, and updates it by 'listening' to event emitters.
-When any emitter emits new values, ObjectState updates its state
+state as a JavaScript object, and updates it by 'listening' to streams or
+event emitters.
+
+When any stream or event emitter emits new values, ObjectState updates its state
 representation, and fires a "data" event. Client code can subscribe to the
 "data" event for a full representation of the state of the system each time
 it changes.
 
+## Example
+
 ```javascript
 var EE = require('events').EventEmitter
 
-var ObjectState = require('objectstate')
+var objectState = require('objectstate')
+  , through = require('through')
+
+var stream = through()
 
 var ee1 = new EE
   , ee2 = new EE
 
-var os = new ObjectState
+var os = objectState()
 
-os.listen(ee1, 'data', ['cat', 'dog'])
-  .listen(ee2, 'error', ['hat'])
+os.listen(stream, 'rat')
+  .listenOn(ee1, 'data', ['cat', 'dog'])
+  .listenOn(ee2, 'error', ['hat'])
 
 os.on('data', function(state) { console.log(state) })
 
-ee1.emit('data', 1)         // {"cat":1}
-ee1.emit('data', 1, 2)      // {"cat":1, "dog":2}
+stream.queue(5)             // {"rat": 5}
+ee1.emit('data', 1)         // {"rat": 5, "cat":1}
+ee1.emit('data', 1, 2)      // {"rat": 5, "cat":1, "dog":2}
 ee2.emit('data', 100)       // does not log, since os does not listen to ee2's
                             // data event.
-ee2.emit('error', "hello")  // {"cat":1, "dog":2, "hat":"hello"}
-ee1.emit('data')            // {"hat":"hello"}
+stream.queue(5)             // does not log, because this changes nothing.
+ee2.emit('error', "hello")  // {"rat": 5, "cat":1, "dog":2, "hat":"hello"}
+ee1.emit('data')            // {"rat": 5, "hat":"hello"}
 ```
+
+## Notes
+
+ObjectState will never alter any object that is passed to it, instead it makes a
+deep copy for use internally. Likewise, it only ever emits a deep copy of its
+state in order to avoid outside mutation.
+
+For performance reasons, deep copy is implemented using
+`JSON.parse(JSON.stringify(state))`, which has a few limitations.
+
+1. It will throw an error if there is a self-reference,
+2. It will have strange side effects on properties that are not `null`,
+   `string`, `boolean`, `array`, or a plain JavaScript `object`.
+   This includes functions, typed arrays, and objects with prototype other than
+   `Object.prototype`.
+
+In practice, these limitations are mostly inconsequential. ObjectState is meant
+to manage **data**, not complicated instances.
+
+ObjectState only ever emits when its internal state changes, a condition
+determined via a deep comparison of the new state object versus the previous
+one.
+
+ObjectState speaks "keypaths", meaning you can reference deeply nested
+properties by dot-separated strings. For example, given the object
+`{animals: {cats: {sound: 'meow'}}}`, `'animals.cats.sound'` would refer to the
+string `'meow'`. If you do not need deep property access, a regular string will
+work as you would expect.
 
 ## API
 
-`ObjectState = require('modules/objectstate')`
-
-Returns a function constructor.
-
-`ObjectState(_initial) -> Stream`
+`objectState(_initial) -> DuplexStream`
 
 Optionally takes an initial state (meaning an object), and returns an
 ObjectState instance.
 
 ObjectState instances are readable/writable streams. ObjectState emits whenever
 its internal state changes, and can be written to with an object to set its
-state.
+internal state.
 
 ### Instance Methods
 
-#### `os.listen(ee, eventName, attributes)`
+#### `os.listen(stream, keypath) -> os`
+- `stream`: A stream
+- `keypath`: The keypath to update on emit
 
- - `ee`: An event emitter
+#### `os.listenOn(ee, eventName, keypaths) -> os`
+- `ee`: An event emitter
+- `eventName`: When `ee` emits events named `eventName`, their objects are
+  recorded on the internal state object.
+- `keypaths`: An array of keypaths.
 
- - `eventName`: When `ee` emits events named `eventName`, their objects are
-   recorded on the internal state object.
+When the `ee` emits the event `eventName`, `ObjectState` saves each argument
+passed to the event handler under the `keypaths` array element at the same
+index. The Nth argument passed to the listener is saved under the Nth element of
+keypaths: `state[keypaths[N]] = arguments[N]`.
 
- - `attributes`: An array of attribute names.
- 
-   When the `ee` emits the event, `eventName`, `ObjectState` saves each
-   argument passed to the event handler under the `attributes` array element at
-   the same index. The i-th argument passed to the listener is saved under the
-   i-th element of attributes: `state[attributes[i]] = arguments[i]`.
+If an emitted argument is undefined, `ObjectState` deletes the corresponding
+keypath in its internal state object.
 
-   If an emitted argument is undefined, `ObjectState` deletes
-   the corresponding attribute in its internal state object.
+If a specified parameter is falsey, it is skipped over during assignment.
 
- - returns itself.
+#### `os.get(keypath)`
 
-#### `os.get(attr)`
+Returns the value for state keypath `keypath` (or `undefined` if not set).
 
-Returns the value for state attribute `attr` (or `undefined` if not set)
+#### `os.set(keypath, value)`
 
-#### `os.set(attr, value)`
+Set the value for keypath `keypath` to `value` on the state.
 
-Set the value for attribute `attr` to `value` on the object state.
+#### `os.remove(keypath)`
 
-#### `os.remove(attr)`
-
-Delete the attribute `attr` from the object state.
+Delete the keypath `keypath` from the state.
 
 #### `os.emitState()`
 
@@ -88,63 +125,13 @@ Emit the current state as a `data` event.
 
 #### `os.wait(fn)`
 
-`fn` is a function that is immediately called. All changes that happen
-during its execution will be collected into a single `data` event, if the state
-changed at all.
+`fn` is a function that is immediately called. All changes that happen during
+its execution will be collected into a single `data` event, if the state changed
+at all.
 
-#### `os.copy()`
+#### `os.state()`
 
-Returns a shallow copy of the instance's current state.
-
-#### `os.deepcopy()`
-
-Returns a deep copy of the instance's current state.
-
-Deep copy is implemented using `JSON.parse(JSON.stringify(state))` which has
-a few limitations.  It will throw an error if there is a self-reference,
-and will have strange side effects on properties that are not `null`, `strings`,
-`booleans`, `arrays`, or plain JavaScript `objects`.  This includes functions,
-typed arrays, and objects with prototype other than `Object.prototype`.
-
-#### `os.snapshot(deep=true)`
-
-**WARNING** This method is deprecated and should not be used. It is scheduled to
-be removed in the next major version.
-
-Returns a function that can be used to reset the object's state. Calling the
-returned restore function will emit a `data` event. 
-
-`deep` specifies whether to shallow-copy or deep-copy the current state.
-
-if the snapshot uses a deep copy, it is subject to the same limitations as
-os.deepcopy.
-
-Usage:
-
-```javascript
-   os = new ObjectState
-   restore = os.snapshot()
-   // time passes...
-   restore()
-```
-
-#### `os.include(otherOS)`
-
-**WARNING** This method is deprecated and should not be used. It is scheduled to
-be removed in the next major version.
-
- - `otherOS`: another objectstate instance
-
-The invoking instance becomes a combination of both itself and the passed
-`objectstate`. Properties from the invoking instance take precedence over
-`otherOS`'s properties.
-
-### Instance Properties
-
-#### `os.state`
-
-A reference to the internal state object which represents the state of the
-system.
+Returns a deep copy of the current state object.
 
 ## License 
 
--- test.js
+++ test.js
@@ -2,306 +2,383 @@
 
 var EE = require('events').EventEmitter
 
-var ObjectState = require('./')
-
 var through = require('through')
   , test = require('tape')
 
-test('ObjectState is a function', function(assert) {
-  assert.equal(typeof ObjectState, 'function')
+var objectState = require('./')
+
+test('is a function', function(assert) {
+  assert.equal(typeof objectState, 'function')
   assert.end()
 })
 
-test('ObjectState is a readable stream', function(assert) {
-  var os = new ObjectState()
+test('is a readable stream', function(assert) {
+  assert.plan(1)
 
-  assert.ok('emit' in os)
-  assert.ok('on' in os)
-  assert.end()
-})
-
-test('Can pipe object state', function(assert) {
-  var ws = new through(write)
+  var ws = through(write)
     , ee = new EE
 
   function write(obj) {
-    assert.deepEqual(obj, {'salutation': 'sup'})
-    assert.end()
+    assert.deepEqual(obj, {salutation: 'sup'})
   }
 
-  var os = new ObjectState()
+  var os = objectState()
 
   os.pipe(ws)
 
-  os.listen(ee, 'data', ['salutation'])
+  os.listenOn(ee, 'data', ['salutation'])
 
   ee.emit('data', 'sup')
 })
 
-test('Can write to object state', function(assert) {
-  var os = new ObjectState
-  var test_state = {cats: true}
+test('can write to object state', function(assert) {
+  assert.plan(1)
+
+  var os = objectState()
+  var testState = {cats: true}
 
   os.on('data', function(state) {
-    assert.deepEqual(state, test_state)
-    assert.deepEqual(os.state, test_state)
-    assert.end()
+    assert.deepEqual(state, testState)
   })
 
-  os.write(test_state)
+  os.write(testState)
 })
 
-test('Is a writable stream', function(assert) {
-  var os = new ObjectState
+test('is a writable stream', function(assert) {
+  assert.plan(1)
+
+  var os = objectState()
   var stream = through()
-  var test_state = {cats: true}
+  var testState = {cats: true}
 
   stream.pipe(os)
 
   os.on('data', function(state) {
-    assert.deepEqual(state, test_state)
-    assert.deepEqual(os.state, test_state)
-    assert.end()
+    assert.deepEqual(state, testState)
   })
 
-  stream.queue(test_state)
+  stream.queue(testState)
 })
 
-test('os.emitState emits current state', function(assert) {
-  var os = new ObjectState({cats: true})
+test('`.emitState()` emits current state', function(assert) {
+  assert.plan(1)
+
+  var os = objectState({cats: true})
 
   os.on('data', function(state) {
     assert.deepEqual(state, {cats: true})
-    assert.end()
   })
 
   os.emitState()
 })
 
-test('ObjectState maps events from emitters to context', function(assert) {
-  var ee_one = new EE()
-    , ee_two = new EE()
+test('`.listen()` listens to streams for attributes', function(assert) {
+  assert.plan(3)
 
-  var os = new ObjectState()
+  var stream = through()
+    , os = objectState()
+    , otherStream = through()
 
-  os.listen(ee_one, 'data', [
+  var cats = ['top cat', 'smarf']
+    , dogs = ['droopy d']
+
+  os.once('data', function(state) {
+    assert.deepEqual(state, {cats: cats})
+  })
+
+  os.listen(stream, 'cats')
+
+  stream.queue(cats)
+
+  os.once('data', function(state) {
+    assert.deepEqual(state, {cats: cats, dogs: dogs})
+  })
+
+  os.listen(otherStream, 'dogs')
+
+  otherStream.queue(dogs)
+
+  otherStream.end()
+
+  os.once('data', function(state) {
+    assert.deepEqual(state, {cats: cats, dogs: dogs})
+  })
+
+  os.emitState()
+})
+
+test('`.listenOn()` maps emitted values to parameters', function(assert) {
+  assert.plan(2)
+
+  var eeOne = new EE()
+    , eeTwo = new EE()
+
+  var os = objectState()
+
+  os.listenOn(eeOne, 'data', [
       'hat'
     , 'bat'
     , 'cat'
   ])
-  os.listen(ee_two, 'data', [
+  os.listenOn(eeTwo, 'data', [
       'rat'
     , null
     , 'cat'
   ])
 
-  ee_one.emit('dne', Math.random())
-
-  assert.deepEqual(os.state, {})
-
-  ee_one.emit('data', 1, 2, 3)
-
-  assert.deepEqual(os.state, {
-      hat: 1
-    , bat: 2
-    , cat: 3
+  os.once('data', function(state) {
+    assert.deepEqual(state, {
+        hat: 1
+      , bat: 2
+      , cat: 3
+    })
   })
 
-  ee_two.emit('data', 4, 5, undefined)
+  eeOne.emit('data', 1, 2, 3)
 
-  assert.deepEqual(os.state, {
-      rat: 4
-    , hat: 1
-    , bat: 2
+  os.once('data', function(state) {
+    assert.deepEqual(state, {
+        rat: 4
+      , hat: 1
+      , bat: 2
+    })
   })
 
-  assert.end()
+  eeTwo.emit('data', 4, 5, undefined)
 })
 
-test('wait will quelch events executed inside of the block', function(assert) {
-  var value_2 = Math.random() + 10
-    , value_1 = Math.random()
-    , os = new ObjectState()
+test('`.wait()` will hold events within given function', function(assert) {
+  var value2 = Math.random() + 10
+    , value1 = Math.random()
+    , os = objectState()
     , ee = new EE()
     , count = 0
 
-  os.listen(ee, 'data', ['value'])
+  os.listenOn(ee, 'data', ['value'])
 
   os.on('data', function(state) {
     ++count
-    assert.equal(state.value, value_2)
+    assert.equal(
+        state.value
+      , value2 + (count === 2 ? 1 : 0)
+    )
   })
 
   os.wait(function() {
-    ee.emit('data', value_1)
-    ee.emit('data', value_2)
+    ee.emit('data', value1)
+    ee.emit('data', value2)
   })
 
   assert.equal(count, 1)
 
-  ee.emit('data', value_2)
+  ee.emit('data', value2 + 1)
 
   assert.equal(count, 2)
 
   assert.end()
 })
 
-test('wait will not send an event if none were emitted', function(assert) {
-  var os = new ObjectState()
+test('`.wait()` sends no event if nothing was emitted', function(assert) {
+  var os = objectState()
     , ee = new EE()
     , count = 0
 
-  os.listen(ee, 'data', ['value'])
-
-  os.on('data', function(state) {
-    ++count
-  })
-
-  os.wait(function() {
-
-  })
-
-  assert.equal(count, 0)
-  assert.end()
-})
-
-test('deepcopy creates deep copy of state', function(assert) {
-  var expect = {}
-    , inner = {}
-    , result
-    , os
-
-  expect.inner = inner
-  expect.inner.a = Math.random()
-  expect.inner.b = [Math.random()]
-  os = new ObjectState(expect)
-  result = os.deepcopy()
-  assert.notStrictEqual(result.inner, expect.inner)
-  assert.notStrictEqual(result, expect)
-  assert.deepEqual(result, expect)
-  assert.end()
-})
-
-test('copy creates shallow copy of state', function(assert) {
-  var expect = {}
-    , inner = {}
-    , result
-    , os
-
-  expect.inner = inner
-  expect.inner.a = Math.random()
-  expect.inner.b = [Math.random()]
-  os = new ObjectState(expect)
-  result = os.copy()
-  assert.notStrictEqual(result, expect)
-  assert.equal(result.inner, expect.inner)
-  assert.deepEqual(result, expect)
-  assert.end()
-})
-
-test('snapshot resets state', function(assert) {
-  var os = new ObjectState()
-    , ee = new EE()
-    , count = 0
-    , expect_0
-    , expect_1
-    , restore
+  os.listenOn(ee, 'data', ['value'])
 
   os.on('data', function() {
     ++count
   })
 
-  expect_0 = Math.random()
-  expect_1 = expect_0 + 1
-  os.listen(ee, 'data', ['value'])
-  ee.emit('data', expect_0)
-  assert.equal(os.state.value, expect_0)
-  restore = os.snapshot()
-  ee.emit('data', expect_1)
-  assert.equal(os.state.value, expect_1)
-  restore()
-  assert.equal(os.state.value, expect_0)
-  assert.equal(count, 3)
+  os.wait(noop)
+
+  assert.equal(count, 0)
   assert.end()
 })
 
-test('include returns other instance of OS', function(assert) {
-  var os_one = new ObjectState()
-    , os_two = new ObjectState()
-
-  assert.equal(os_one.include(os_two), os_two)
-  assert.end()
-})
-
-test('include puts os_b\'s properties on os_a; chains', function(assert) {
-  var os_a = new ObjectState()
-    , os_b = new ObjectState()
-    , os_c = new ObjectState()
-    , ee = new EE()
-
-  os_a.include(os_b).include(os_c)
-  os_c.listen(ee, 'foo', ['c'])
-  os_b.listen(ee, 'foo', ['b'])
-  os_a.listen(ee, 'boo', ['a'])
-  ee.emit('boo', 2)
-  assert.equal(os_a.state.a, 2)
-  assert.equal(os_a.state.b, undefined)
-  assert.equal(os_a.state.c, undefined)
-  assert.equal(os_b.state.a, undefined)
-  assert.equal(os_b.state.b, undefined)
-  assert.equal(os_b.state.c, undefined)
-  assert.equal(os_c.state.a, undefined)
-  assert.equal(os_c.state.b, undefined)
-  assert.equal(os_c.state.c, undefined)
-  ee.emit('foo', 3)
-  assert.equal(os_a.state.a, 2)
-  assert.equal(os_a.state.b, 3)
-  assert.equal(os_a.state.c, 3)
-  assert.equal(os_b.state.a, undefined)
-  assert.equal(os_b.state.b, 3)
-  assert.equal(os_b.state.c, 3)
-  assert.equal(os_c.state.a, undefined)
-  assert.equal(os_c.state.b, undefined)
-  assert.equal(os_c.state.c, 3)
-  assert.end()
-})
-
-test('can set attr via `.set` method', function(assert) {
+test('does not emit if value is unchanged', function(assert) {
   assert.plan(1)
 
-  var os = new ObjectState()
+  var os = objectState()
+    , ee = new EE
+    , count = 0
 
-  os.on('data', function(state) {
+  os.listen(ee, 'cats')
+
+  os.on('data', function() {
+    ++count
+  })
+
+  ee.emit('data', true)
+  ee.emit('data', true)
+
+  os.set('cats', true)
+
+  os.write({cats: true})
+
+  os.remove('dogs')
+
+  assert.equal(count, 1)
+})
+
+test('always emits copies', function(assert) {
+  assert.plan(4)
+
+  var original = {cats: true}
+    , written = {cats: false}
+
+  var os = objectState(original)
+
+  os.once('data', function(state) {
+    // verifies that the state and original do not share a reference,
+    // but that they are identical otherwise.
+    assert.notEqual(state, original)
+    assert.deepEqual(state, original)
+  })
+
+  os.emitState()
+
+  os.once('data', function(state) {
+    assert.notEqual(state, written)
+    assert.deepEqual(state, written)
+  })
+
+  os.write(written)
+})
+
+test('`.state()` returns deep copy of state', function(assert) {
+  assert.plan(3)
+
+  var expect = {}
+    , inner = {}
+    , result
+    , os
+
+  expect.inner = inner
+  expect.inner.a = Math.random()
+  expect.inner.b = [Math.random()]
+  os = objectState(expect)
+  result = os.state()
+  assert.notEqual(result.inner, expect.inner)
+  assert.notEqual(result, expect)
+  assert.deepEqual(result, expect)
+})
+
+test('emits a deep-copy of state', function(assert) {
+  assert.plan(4)
+
+  var original = {cats: true}
+    , result2
+    , result
+    , os
+
+  os = objectState(original)
+
+  os.once('data', function(state) {
+    result = state
+  })
+
+  os.emitState()
+
+  assert.deepEqual(original, result)
+  assert.notEqual(original, result)
+
+  os.once('data', function(state) {
+    result2 = state
+  })
+
+  os.emitState()
+
+  assert.deepEqual(result, result2)
+  assert.notEqual(result, result2)
+})
+
+test('deep copies on write', function(assert) {
+  assert.plan(2)
+
+  var written = {cats: {hats: true}}
+    , os = objectState()
+
+  os.write(written)
+
+  os.set('cats.hats', false)
+
+  assert.equal(written.cats.hats, true)
+  assert.equal(os.get('cats.hats'), false)
+})
+
+test('can nest `.wait()` statements', function(assert) {
+  assert.plan(1)
+
+  var os = objectState()
+    , stream = through()
+    , count = 0
+
+  os.listen(stream, 'herp')
+
+  os.on('data', function() {
+    ++count
+  })
+
+  os.wait(function() {
+    os.wait(function() {
+      stream.queue('flerp')
+      stream.queue('werp')
+    })
+
+    stream.queue('herp')
+    stream.queue('merp')
+  })
+
+  assert.equal(count, 1)
+})
+
+test('can set attr via `.set()` method', function(assert) {
+  assert.plan(2)
+
+  var os = objectState()
+
+  os.once('data', function(state) {
     assert.deepEqual(state, {herp: 'derp'})
   })
 
   os.set('herp', 'derp')
+
+  os.once('data', function(state) {
+    assert.deepEqual(state, {herp: 'derp', derp: {herp: 'flerp'}})
+  })
+
+  os.set('derp.herp', 'flerp')
 })
 
-test('can get attr vi `.get` method', function(assert) {
-  assert.plan(3)
+test('can get attr via `.get()` method', function(assert) {
+  assert.plan(4)
 
-  var os = new ObjectState({a: false, b: '1'})
+  var os = objectState({a: false, b: '1', d: {hey: 'cats'}})
 
   assert.equal(os.get('a'), false)
   assert.equal(os.get('b'), '1')
   assert.equal(os.get('c'), undefined)
+  assert.equal(os.get('d.hey'), 'cats')
 })
 
-test('can remove an attr from object state', function(assert) {
-  assert.plan(3)
+test('can remove an attr via `.remove()`', function(assert) {
+  assert.plan(2)
 
-  var os = new ObjectState({'why': 'yes'})
+  var os = objectState({why: 'yes', no: {sure: 'whynot'}})
 
-  os.on('delete-key', function(key) {
-    assert.equal(key, 'why')
-  })
-
-  os.on('data', function(state) {
-    assert.deepEqual(state, {})
+  os.once('data', function(state) {
+    assert.deepEqual(state, {no: {sure: 'whynot'}})
   })
 
   os.remove('why')
 
-  assert.ok(!os.state.hasOwnProperty('why'), 'removed the property')
+  os.once('data', function(state) {
+    assert.deepEqual(state, {no: {}})
+  })
+
+  os.remove('no.sure')
 })
+
+function noop() {
+  // no-op
+}
